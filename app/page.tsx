8147bba4c2915e3d361d32630a5b6d62bdd10136
priya <priyaasinghh345@gmail.com>
2026-06-15 08:23:08 +0530

    make async server component to access data

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,25 @@
 import Image from "next/image";
 
-export default function Home() {
+async function getUserDetails() {
+  const response = await fetch("https://week-13-offline.kirattechnologies.workers.dev/api/v1/user/details")
+  return response.json();
+}
+
+// async server component
+export default async function Home() {
+  await new Promise((r) => setTimeout(r, 1000));
+  const userDetails = await getUserDetails();
+
   return (
-    <div>
-      Root route
+    <div className="flex flex-col justify-center h-screen">
+      <div className="flex justify-center">
+        <div className="border p-8 rounded">
+          <div>
+            Name: `Priiii`,${userDetails.name}
+          </div>
+          {userDetails.email}
+        </div>
+      </div>
     </div>
   );
 }
