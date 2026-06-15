b0df3b095ab4b84d396447f228545f75fff98f5e
priya <priyaasinghh345@gmail.com>
2026-06-15 08:23:47 +0530

    add backend route

---
+++ app/api/user/route.ts
@@ -0,0 +1,15 @@
+
+// GET route should be named GET
+export function GET (){
+    return Response.json({
+        name: "Priiii",
+        email: "priiii@example.com"
+    })
+}
+
+export function POST (){
+    return Response.json({
+        name: "Priiii",
+        email: "priiii@example.com"
+    })
+}
