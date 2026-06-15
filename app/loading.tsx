ef37892dd5c347d9d6a0d8247ed65b2133a5cc32
priya <priyaasinghh345@gmail.com>
2026-06-15 08:23:28 +0530

    add shimmer ui till data is being fetched

---
+++ app/loading.tsx
@@ -0,0 +1,15 @@
+const Loading = () => {
+    return (
+        <div className="flex flex-col justify-center h-screen">
+            <div className="flex justify-center">
+                <div className="border p-8 rounded">
+                    <div>
+                        Loading...
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Loading;
