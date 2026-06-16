33acc88ba0a6bbefbab57187422cd7d07eb4b671
priya <priyaasinghh345@gmail.com>
2026-06-16 12:08:33 +0530

    add req o post api route

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -1,15 +1,17 @@
+import { NextRequest } from "next/server";
 
 // GET route should be named GET
-export function GET (){
-    return Response.json({
-        name: "Priya Singh",
-        email: "priyasingh@example.com"
-    })
+export function GET() {
+  return Response.json({
+    name: "Priya Singh",
+    email: "priyasingh@example.com",
+  });
 }
 
-export function POST (){
-    return Response.json({
-        name: "Priya Singh",
-        email: "priyasingh@example.com"
-    })
+export async function POST(req: NextRequest) {
+  const { username, password } = await req.json();
+  return Response.json({
+    name: "Priya Singh",
+    email: "priyasingh@example.com",
+  });
 }
